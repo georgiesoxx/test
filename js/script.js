ba3bd50dd37b62794d216fe15f80bed805510c73
georgiesoxx <jadecowling@gmail.com>
2017-02-19 19:10:42 +0000

    included some validation and enter to search

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 var venueObj={};
 
-function search(){
-$.getJSON('https://api.foursquare.com/v2/venues/search?ll=44.3,37.2&near='+$('#locationTxt').val()+'&oauth_token=QLG3JFV1VK2JGZ2E0S3YUAXFLSCOFW0VYT43SHSB5EGBWOKD&v=20170219', function(data) {
-  venueObj=data;
-  console.log(venueObj)
-});
+$(document).ready(function() {
+  $('#locationTxt').keyup(function(e){
+    if(e.keyCode == 13){
+      searchForVenues();
+    }
+  });
+})
+
+function searchForVenues(){
+  if($('#locationTxt').val().trim()== ""){
+    console.log("no search criteria entered")
+  } else {
+  $.getJSON('https://api.foursquare.com/v2/venues/search?ll=44.3,37.2&near='+$('#locationTxt').val()+'&oauth_token=QLG3JFV1VK2JGZ2E0S3YUAXFLSCOFW0VYT43SHSB5EGBWOKD&v=20170219', function(data) {
+      venueObj=data;
+      console.log(venueObj)
+    })
+    .fail(function() {
+      console.log("location doesn't exist");
+   })
+  }
 }
