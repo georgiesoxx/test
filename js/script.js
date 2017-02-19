0b3a57215a0f36d769c8585c6e4f56e6e8810d7d
georgiesoxx <jadecowling@gmail.com>
2017-02-19 19:41:24 +0000

    displaying search results, simple styling

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ function searchForVenues(){
   $.getJSON('https://api.foursquare.com/v2/venues/search?ll=44.3,37.2&near='+$('#locationTxt').val()+'&oauth_token=QLG3JFV1VK2JGZ2E0S3YUAXFLSCOFW0VYT43SHSB5EGBWOKD&v=20170219', function(data) {
       venueObj=data;
       console.log(venueObj)
+      $("#venueResults ul").empty();
+      for(var i=0; i<venueObj.response.venues.length; i++){
+        var venueLi = $("<li>", {"text": venueObj.response.venues[i].name});
+        $("#venueResults ul").append(venueLi)
+      }
+
     })
     .fail(function() {
       console.log("location doesn't exist");
